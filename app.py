2a185f927bd4ac4ad37895b100c1852d383da860
unknown <lambilat@protonmail.com>
2023-03-12 17:12:01 +0800

    good

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-import os,sys
+import os
 from mediafire.client import (MediaFireClient, File, Folder)
 from flask import Flask, request,Response
 #from dotenv import load_dotenv
 #load_dotenv()
 app = Flask(__name__)
 
+
+
 try:
     client = MediaFireClient()
-    client.login(email=os.environ.get('mediafire_email'),
-        password=os.environ.get('mediafire_pass'),
+    client.login(email=os.getenv('mediafire_email'),
+        password=os.getenv("mediafire_pass"),
         app_id="42511"
         )
 except Exception as e:
+    print("Error:",e)
     client = None
 
 
@@ -21,7 +24,7 @@ mediafire_files = []
 @app.route('/files')
 def get_files_page():
     global mediafire_files
-    if os.environ.get('getfiles') == "1":
+    if os.getenv('getfiles') == "1":
         print(mediafire_files)
         mediafire_files = []
         print("File Cleared")
@@ -32,9 +35,11 @@ def root_page():
     global mediafire_files
 
     ytid = request.args.get("ytid")
+    resp_txt = "<br> Error <br>"
     isgood = False
     if ytid and client:
         print("Youtube ID:",ytid)
+        resp_txt += " Youtube ID: "+ytid+"<br>"
         if not mediafire_files:
             try:
                 for item in client.get_folder_contents_iter("mf:/karaoke"):
@@ -42,25 +47,29 @@ def root_page():
                         mediafire_files.append(item['filename'])
                         #print("File: {}".format(item['filename']))
             except Exception as e:
+                resp_txt += str(e)+"<br>"
                 pass
 
 
         if mediafire_files:
             if ytid in mediafire_files:
+                resp_txt += " File Found<br>"
                 print("File Found, Getting DL Url.")
                 try:
                     dl_url = client.get_file_dl_url("mf:/karaoke/"+ytid)
                     if dl_url:
                         isgood = True
                 except Exception as e:
+                    resp_txt += str(e)+"<br>"
                     pass
             else:
+                resp_txt += " File Not Found<br>"
                 print("File Not Found")
 
     if isgood:
         return Response(dl_url, status=201, mimetype="text/plain")
     else:
-        return Response("Error Getting Url.", status=404, mimetype="text/plain")
+        return Response(resp_txt, status=404, mimetype="text/plain")
 
     #print(ytid)
     #return Response("Hello World!", status=201, mimetype="text/plain")
