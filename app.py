211afca1bbf79f06a0cf19746016badbff5338b3
unknown <lambilat@protonmail.com>
2023-03-12 17:32:01 +0800

    last

--- app.py
+++ app.py
@@ -7,15 +7,21 @@ app = Flask(__name__)
 
 
 
-try:
-    client = MediaFireClient()
-    client.login(email=os.getenv('mediafire_email'),
-        password=os.getenv("mediafire_pass"),
-        app_id="42511"
-        )
-except Exception as e:
-    print("Error:",e)
-    client = None
+client = None
+def start_mediafire_client():
+    global client
+    try:
+        client = MediaFireClient()
+        client.login(email=os.getenv('mediafire_email'),
+            password=os.getenv("mediafire_pass"),
+            app_id="42511"
+            )
+    except Exception as e:
+        client = None
+        return str(e)
+start_mediafire_client()
+
+
 
 
 mediafire_files = []
@@ -24,6 +30,9 @@ mediafire_files = []
 @app.route('/files')
 def get_files_page():
     global mediafire_files
+    if not client:
+        start_mediafire_client_resp = start_mediafire_client()
+        
     if os.getenv('getfiles') == "1":
         print(mediafire_files)
         mediafire_files = []
@@ -34,14 +43,21 @@ def get_files_page():
 def root_page():
     global mediafire_files
 
+    resp_err_txt = "<br> Error <br>"
+
+    if not client:
+        start_mediafire_client_resp = start_mediafire_client()
+        if start_mediafire_client_resp:
+            " Mediafire Client: "+start_mediafire_client_resp+"<br>"
+
+
     ytid = request.args.get("ytid")
-    resp_txt = "<br> Error <br>"
-    resp_txt += " Youtube ID: "+request.args.get("ytid")+"<br>"
-    resp_txt += " Mediafire Client: "+str(client)+"<br>"
+
+    resp_err_txt += " Youtube ID: "+request.args.get("ytid")+"<br>"
     isgood = False
     if ytid and client:
         print("Youtube ID:",ytid)
-        resp_txt += " Youtube ID: "+ytid+"<br>"
+        #resp_err_txt += " Youtube ID: "+ytid+"<br>"
         if not mediafire_files:
             try:
                 for item in client.get_folder_contents_iter("mf:/karaoke"):
@@ -49,29 +65,29 @@ def root_page():
                         mediafire_files.append(item['filename'])
                         #print("File: {}".format(item['filename']))
             except Exception as e:
-                resp_txt += str(e)+"<br>"
+                resp_err_txt += str(e)+"<br>"
                 pass
 
 
         if mediafire_files:
             if ytid in mediafire_files:
-                resp_txt += " File Found<br>"
+                resp_err_txt += " File Found<br>"
                 print("File Found, Getting DL Url.")
                 try:
                     dl_url = client.get_file_dl_url("mf:/karaoke/"+ytid)
                     if dl_url:
                         isgood = True
                 except Exception as e:
-                    resp_txt += str(e)+"<br>"
+                    resp_err_txt += str(e)+"<br>"
                     pass
             else:
-                resp_txt += " File Not Found<br>"
+                resp_err_txt += " File Not Found<br>"
                 print("File Not Found")
 
     if isgood:
         return Response(dl_url, status=201, mimetype="text/plain")
     else:
-        return Response(resp_txt, status=404, mimetype="text/plain")
+        return Response(resp_err_txt, status=404, mimetype="text/plain")
 
     #print(ytid)
     #return Response("Hello World!", status=201, mimetype="text/plain")
